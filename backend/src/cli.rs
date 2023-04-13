2f8d60e865c15d7ad0805d60981eb053c5b57b5e
Sakura1943 <1436700265@qq.com>
2023-04-13 23:38:35 +0800

    update backend CLI help and add frontend font

--- backend/src/cli.rs
+++ backend/src/cli.rs
@@ -3,8 +3,10 @@ use clap::{Parser, CommandFactory, Command};
 #[derive(Debug, Parser)]
 #[command(version, author, about, long_about = None)]
 pub struct Cli {
+    /// Server port
     #[arg(short, long, default_value_t = 3000)]
     pub port: u16,
+    /// Server host
     #[arg(short = 's', long, default_value = "127.0.0.1")]
     pub host: String
 }
